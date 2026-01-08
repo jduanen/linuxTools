c8021965c004dc4d6fdcc3ab0b4b78deeb5b0d8e
J. Duane Northcutt <duane.northcutt@gmail.com>
2026-01-08 14:42:22 -0800

    Created.

---
+++ bin/ollamaList.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Script to search for ollama models that can be pulled by a ollama container
+#
+
+USAGE="$0 <modelPrefix>"  ## matches models starting with the given string
+
+####curl -s https://registry.ollama.ai/v2/library/ | jq -r 'keys[]'
+
+if [ $# -eq 0 ]; then
+    # get all of the models and sort them alphabetically
+    curl -s https://ollama.com/library | grep -oP 'href="/library/[^"/]+' | sed 's|href="/library/||; s|/.*||g' | sort -u
+elif [ $# -eq 1 ]; then
+    MODEL_PREFIX=$1
+    curl -s https://ollama.com/library | grep -oP 'href="/library/'${MODEL_PREFIX}'[^"/]+' | sed 's|href="/library/||; s|/.*||g' | sort -u
+else
+    echo "ERROR: extra arguments"
+    echo ${USAGE}
+    exit 1
+fi
