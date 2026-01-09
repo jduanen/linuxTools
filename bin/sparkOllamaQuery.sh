32116aecabf58fff2037dce9c0f936a0f054191d
J. Duane Northcutt <duane.northcutt@gmail.com>
2026-01-08 17:50:55 -0800

    Added options.

--- bin/sparkOllamaQuery.sh
+++ bin/sparkOllamaQuery.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 #
-# This script issues a query to the desired local LLM model running on the DGX spark
+# This script issues a query to the desired LLM model running on the DGX spark
 #
-# Usage: ./localLLM.sh <query> {"stream"}
+# Usage: ./sparkOllamaQuery.sh <query> {<model> {"stream"}}
 
 #### TODO think about output to a file as an option
-#### TODO parse the response JSON from CURL and assemble the full response message
+#### TODO parse the response JSON from CURL (with jq) and assemble the full response message
 
+DEF_MODEL="gpt-oss:20b"
 SERVER="spark-8d0d.lan"
-MODEL="llama3.1:8b"
+
+USAGE="Usage: $0  {<model> {\"stream\"}} <query>"
+
+MODEL=${DEF_MODEL}
 STREAM=false
 
 if [ $# -eq 0 ] || [ $# -gt 2 ]; then
-  echo "Usage: ./localLLM.sh <query> {"stream"}"
+  echo ${USAGE}
   exit 1
-fi
-
-CONTENT=$1
-
-STREAM="false"
-if [ $# -eq 2 ]; then
+elif [ $# -eq 1 ]; then
+  CONTENT="$1"
+elif [ $# -eq 2 ]; then
+  MODEL="$1"
+  CONTENT="$2"
+elif [ $# -eq 3 ]; then
+  MODEL="$1"
   if [ $2 == "stream" ]; then
     STREAM="true"
   else
-    echo "Usage: ./localLLM.sh <query> {"stream"}"
+    echo ${USAGE}
     exit 1
   fi
+  CONTENT="$3"
 fi
 
-curl "http://${SERVER}:11434/api/chat" -d "{\"model\": \"${MODEL}\", \"messages\": [{\"role\": \"user\", \"content\": \"$CONTENT\"}],
-  \"stream\": ${STREAM}}"
+curl -s "http://${SERVER}:11434/api/chat" -d "{\"model\": \"${MODEL}\", \"messages\": [{\"role\": \"user\", \"content\": \"$CONTENT\"}],
+  \"stream\": ${STREAM}}" | jq -r .
 
