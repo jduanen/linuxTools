968d9ca43b9d3c63c0f5b363325382dcc6ed32c3
Duane Northcutt <duane.northcutt@gmail.com>
2026-01-15 17:38:44 -0800

    Added comments.

--- scripts/startPortainerAgent.sh
+++ scripts/startPortainerAgent.sh
@@ -3,7 +3,10 @@
 # Script to start Portainer Agent on a machine to be monitored.
 # This is a lightweight agent that runs on machines to be monitored.
 #
-# Remote Use: <browser> https://<primaryHostName>:9443
+# Add agent as remote environment to the manager.
+# On the Portainer WebUI, got to Environments->Docker standalone->Agent and
+#  give remote the host's name and agent URL (http://gpuServer1.lan:9001), then
+#  click Connect.
 
 docker run -d -p 9001:9001 --name portainer_agent --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
