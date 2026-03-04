f765a73da39919b286c48b16b5f5aff95e14a2a9
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-04 13:19:46 -0800

    Created.

---
+++ scripts/startOpenWebUI.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Script to start OpenWebUI in a container on a host linux machine.
+
+docker run -d \
+  -p 3000:8080 \
+  -v open-webui:/app/backend/data \
+  --name open-webui \
+  --restart always \
+  ghcr.io/open-webui/open-webui:main
