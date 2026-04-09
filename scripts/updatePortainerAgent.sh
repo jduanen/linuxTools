af1fbaa57bf8edcf35f54809957924a463dc07aa
Duane Northcutt <duane.northcutt@gmail.com>
2026-04-09 16:19:11 -0700

    Fixed script path.

--- scripts/updatePortainerAgent.sh
+++ scripts/updatePortainerAgent.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Script to update the Portainer agent instance
 
-docker stop portainer
-docker rm portainer
+
+SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+
+${SCRIPT_PATH}/stopPortainerAgent.sh
+
 docker pull portainer/portainer-ce:latest
 
-SCRIPT_PATH=$(realpath "$0")
 ${SCRIPT_PATH}/startPortainerAgent.sh
