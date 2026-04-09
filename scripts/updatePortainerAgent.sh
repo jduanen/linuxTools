ad5842e1915360ef94a44d9092efc2aedee76249
Duane Northcutt <duane.northcutt@gmail.com>
2026-04-09 16:27:12 -0700

    Fixed image.

--- scripts/updatePortainerAgent.sh
+++ scripts/updatePortainerAgent.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Script to update the Portainer agent instance
-
+#
+# Script to update the local Portainer agent instance
 
 SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 
 ${SCRIPT_PATH}/stopPortainerAgent.sh
 
-docker pull portainer/portainer-ce:latest
+docker pull portainer/agent:latest
 
 ${SCRIPT_PATH}/startPortainerAgent.sh
